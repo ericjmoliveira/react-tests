fba8f045adc82b47a7d3cea21cc9a8a1bc078f2f
Eric Oliveira <ericjmoliveira@gmail.com>
2023-05-02 20:12:29 -0300

    Write the second app component test

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,5 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
+// import {} from '@testing-library/user-event'
 
 import App from './App';
 
@@ -8,6 +9,16 @@ describe('App component', () => {
     const { getByText } = render(<App />);
 
     expect(getByText('Hello world!')).toBeInTheDocument();
+
+    screen.debug();
+  });
+
+  it('should render the list of languages', () => {
+    const { getByText } = render(<App />);
+
+    expect(getByText('HTML')).toBeInTheDocument();
+    expect(getByText('CSS')).toBeInTheDocument();
+    expect(getByText('JavaScript')).toBeInTheDocument();
 
     screen.debug();
   });
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { useState } from 'react';
+
 export default function App() {
-  return <h1>Hello world!</h1>;
+  const [languagesList, setLanguagesList] = useState(['HTML', 'CSS', 'JavaScript']);
+
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <h2>Languages:</h2>
+      <ul>
+        {languagesList.map((lang) => (
+          <li key={lang}>{lang}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
